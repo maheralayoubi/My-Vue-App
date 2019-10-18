7b5a1eb52a353882567804ed555cd26f93d25afc
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-10-18 23:08:44 +0900

    Making the first coding step

---
+++ src/components/Calculator.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="calculator">
+    <div class="display">43857</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+
+<style scoped>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto)
+}
+.display {
+  background-color: tomato;
+  grid-column: 1/5;
+}
+.zero {
+  grid-column: 1/3;
+}
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
+}
+</style>
