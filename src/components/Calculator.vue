d4692ce3d2d69d9d9e2c5e93292f423a8ac230dc
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-10-19 16:27:46 +0900

    Adding more functions

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,22 +2,22 @@
   <div class="calculator">
     <div class="display">{{current || '0'}}</div>
     <div @click="clear" class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
+    <div @click="sign"  class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
     <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
+    <div @click="append('0')" class="btn zero">0</div>
     <div class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
@@ -33,6 +33,16 @@ export default {
   methods: {
     clear() {
       this.current = '';
+    },
+    sign() {
+      // if the first charechter is equal to "-" return this string without that charechter otherwise return the string with "-" sign
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      this.current = `${this.current}${number}`
     }
   }
 }
